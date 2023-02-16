ceeb2b716b7bb9da1e8e895abd116805c6a47ff1
Oyewo Bolaji Sodiq <oyewob@gmail.com>
2023-02-16 07:23:31 -0800

    solved betty warnings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: prints "Programming is like building a multilingual puzzle"
  *
- * Return Always 0 (Succes)
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
